a1b9b4b6a33135337928ffa981d6af7b1a0d8985
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-02 15:42:26 -0400

    login logout sekleton

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/login', function(req, res) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/logout', function(req, res) {
   res.send('respond with a resource');
 });
 
