cfd81c86a912acb129f6b8734f586f1df94b6f86
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-03 13:25:51 -0400

    new connection string

--- src/db.js
+++ src/db.js
@@ -5,9 +5,17 @@ var options = {
   promiseLib: promise
 };
 
+const config = {
+  host: 'localhost',
+  port: 5432,
+  database: 'draw',
+  user: 'jzhong',
+  password: 'testtest',
+  poolSize: 10
+}
+
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/draw';
-var db = pgp(connectionString);
+var db = pgp(config);
 
 monitor.attach(options); // attach to all events at once;
 
