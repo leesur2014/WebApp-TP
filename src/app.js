5c5894cc3f0f6c76c6275b14f388912fc595e2e2
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-03 13:11:16 -0400

    add redis and express-ws

--- src/app.js
+++ src/app.js
@@ -1,23 +1,22 @@
 'use strict';
 
 var express = require('express');
+var redis = require('redis');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var api = require('./routes/api');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var expressWs = require('express-ws')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
