643c5b864e35e0903f90ecc34e891af66cd9b2b2
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-04 12:53:25 -0400

    test data sql

---
+++ src/testdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (fb_id, nickname) VALUES ('test_id_2', 'Test User 2') RETURNING id;
+INSERT INTO users (fb_id, nickname) VALUES ('test_id_3', 'Test User 3') RETURNING id;
+INSERT INTO users (fb_id, nickname) VALUES ('test_id_1', 'Test User 1') RETURNING id;
