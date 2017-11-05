ef59370cd1ce8a23d55a5a37d0f7c314d5e4080a
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-05 11:04:30 -0500

    can login via FB

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/login', function(req, res) {
-  res.send('respond with a resource');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FB_APP_ID,
+    clientSecret: process.env.FB_APP_SECRET,
+    callbackURL: process.env.FB_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      console.log(profile);
+      return done(null, profile);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
+
+passport.deserializeUser(function(id, done) {
+  console.log("deserialize " + id);
+  done(null, {hello: "world"})
+  // User.findById(id, function(err, user) {
+  //   done(err, user);
+  // });
+});
+
+router.get('/login', passport.authenticate('facebook'));
+
+router.get('/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/user/login' }));
 
 
 router.get('/logout', function(req, res) {
