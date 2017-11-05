ecc113c428acc59f307593cbe3f7261a00e4cec9
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-05 13:03:37 -0500

    commented out unused cols

--- src/schema.sql
+++ src/schema.sql
@@ -6,9 +6,9 @@ $$ language sql;
 CREATE TABLE "users" (
 	"id" SERIAL NOT NULL,
 	"fb_id" VARCHAR(255) NOT NULL UNIQUE,
-	"fb_access_token" CHAR(255) NULL,
-	"fb_token_expire_at" TIMESTAMP WITHOUT TIME ZONE NULL,
-	"access_token" CHAR(128) NULL,
+	-- "fb_access_token" CHAR(255) NULL,
+	-- "fb_token_expire_at" TIMESTAMP WITHOUT TIME ZONE NULL,
+	-- "access_token" CHAR(128) NULL,
 	"nickname" VARCHAR(64) NOT NULL,
 	"score_draw" INTEGER NOT NULL DEFAULT '0',
 	"score_guess" INTEGER NOT NULL DEFAULT '0',
