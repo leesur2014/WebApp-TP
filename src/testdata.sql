806d3f0af3dac4bb91eeabb41793f534b7113d92
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-05 08:03:30 -0500

    add data for painter_score_map

--- src/testdata.sql
+++ src/testdata.sql
@@ -4,3 +4,6 @@ INSERT INTO users (fb_id, nickname) VALUES ('test_id_1', 'Test User 1') RETURNIN
 
 
 INSERT INTO dictionary VALUES ('apple'), ('orange'), ('greet'), ('dictionary'), ('keyboard');
+
+
+INSERT INTO "painter_score_map" VALUES (0, 0), (1, 3), (2, 3), (3, 2), (4, 1), (5, 1);
