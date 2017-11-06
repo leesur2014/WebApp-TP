3299e4994f74fc120dbbe9229655b3caa4ed46e4
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-05 22:28:46 -0500

    Round.end() , Round.abort()

--- src/models/round.js
+++ src/models/round.js
@@ -1,11 +1,16 @@
 var db = require('./db');
+var redis = require('./redis').pubClient;
 
 Round = {};
 
 Round.end = function (round_id) {
-  return db.many('SELECT * FROM round_end($1)', round_id)
-    .then(function (data) {
-      // can we remove this function?
+  return db.proc('round_end', round_id)
+    .then(function (round) {
+      var evt = {
+        event: "round_end",
+        data: round
+      };
+      redis.publish("room:" + round.room_id, JSON.stringify(evt));
       return data;
     })
     .catch(function (err) {
@@ -13,3 +18,24 @@ Round.end = function (round_id) {
       return null;
     });
 };
+
+
+
+Round.abort = function (round_id) {
+  return db.proc('round_abort', round_id)
+    .then(function (round) {
+      var evt = {
+        event: "round_abort",
+        data: round
+      };
+      redis.publish("room:" + round.room_id, JSON.stringify(evt));
+      return data;
+    })
+    .catch(function (err) {
+      console.log(err);
+      return null;
+    });
+};
+
+
+module.exports = Round;
