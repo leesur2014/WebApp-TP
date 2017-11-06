08ed0421a02655c227811e2ed4d776bb18b8083a
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-05 21:58:31 -0500

    redis connector

---
+++ src/models/redis.js
@@ -0,0 +1,25 @@
+var redis = require("redis");
+
+var client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
+});
+
+function logError(err) {
+  console.log("redis error: " + err);
+}
+
+client.on("error", logError);
+
+module.exports.pubClient = client;
+
+module.exports.createClient = function (){
+  var client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  });
+
+  client.on("error", logError);
+
+  return client
+};
