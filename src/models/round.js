dc2d41daf4ce5f3dfbd499690c47c116580419a5
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-11 15:53:21 -0500

    add getLatestCanvas

--- src/models/round.js
+++ src/models/round.js
@@ -8,18 +8,24 @@ Round.end = function (round_id) {
     .then(function (round) {
       io.to('room_' + round.room_id)
         .emit('round_end', {round_id: round_id});
+      return null;
     })
     .catch(function (err) {
       // this is an error
       console.log(err);
+      return null;
     });
 };
 
 Round.getInfoById = async function (id) {
-  let round;
-  round = await db.proc("round_get_by_id", [id]);
-  round.users = await db.any('SELECT user_id, score FROM round_user WHERE round_id = $1', id);
-  return round;
+  return db.proc("round_get_by_id", [id])
+    .then(function (round) {
+      return db.any('SELECT user_id, score FROM round_user WHERE round_id = $1', id)
+        .then(function (data) {
+          round.users = data;
+          return round;
+        });
+    });
 }
 
 Round.abort = function (round_id) {
@@ -27,12 +33,18 @@ Round.abort = function (round_id) {
     .then(function (round) {
       io.to('room_' + round.room_id)
         .emit('round_end', {round_id: round_id});
+      return null;
     })
     .catch(function (err) {
       // this is not an error
       console.log(err);
+      return null;
     });
 };
+
+Round.getLatestCanvas = function (round_id) {
+  return db.proc('round_get_latest_canvas', round_id, c => c.round_get_latest_canvas);
+}
 
 
 module.exports = Round;
