1a8015898f413951da8fbec18e3a1dee825f6c65
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-11 15:53:39 -0500

    rewrite with async/await

--- src/models/room.js
+++ src/models/room.js
@@ -16,15 +16,11 @@ Room.getPublicRoomById = function (room_id) {
   return db.one("SELECT * FROM public_rooms WHERE id = $1", room_id);
 }
 
-Room.getInfoById = function (room_id) {
-  return db.proc("room_get_by_id", room_id)
-    .then(function (room) {
-      db.any('SELECT id, nickname, observer, ready FROM room_get_users($1)', room.id)
-        .then(function (users) {
-          room.users = users;
-          return room;
-        });
-    });
+Room.getInfoById = async function (room_id) {
+  let room = await db.proc("room_get_by_id", room_id)
+  room.users = await db.any('SELECT id, nickname, observer, ready FROM room_get_users($1)', room.id)
+  room.round_id = await db.proc('room_get_current_round', room.id, r => r.id);
+  return room;
 };
 
 Room.startNewRound = function (room_id) {
@@ -32,10 +28,12 @@ Room.startNewRound = function (room_id) {
     .then(function (round) {
       io.to('room_' + room_id)
         .emit('round_start', {round_id: round.id});
+      return null;
     })
     .catch(function (err) {
       // this is not an error
       console.log(err);
+      return null;
     });
 };
 
