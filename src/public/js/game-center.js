8d08a2828016dfad56a14f79920dd8495a14383d
leesur2014 <542785042@qq.com>
2017-11-12 10:25:32 -0500

    Before canvas.

--- src/public/js/game-center.js
+++ src/public/js/game-center.js
@@ -1,4 +1,13 @@
 $(function() {
+    var node = document.createElement("div");
+    var player_count = 4;//data['data'][i].user_count;
+    node.innerHTML = "<img src=\"/static/images/game-table/"+String(player_count)+".png\" class=\"img\">";
+    var text = document.createElement("div");
+    var room_id = 1;
+    text.innerHTML = "<a id=\"room_id\">Room"+String(room_id)+"</a>";
+    node.append(text);
+    $('#roomContainer').append(node); 
+    /*
     $.get('/api/lounge', function(data) {
         console.log(JSON.stringify(data));
         var l = data['data'].length;
@@ -20,5 +29,6 @@ $(function() {
     .fail(function() {
         alert( "error" );
     });
+    */
 });
 
--- src/public/js/room.js
+++ src/public/js/room.js
@@ -1,10 +1,58 @@
 $(function() {
+    var data = {
+    "code": 0,
+    "data": {
+        "id": 1,
+        "passcode": "          ",
+        "created_at": "2017-11-05T23:43:53.451Z",
+        "deleted_at": null,
+        "users": [
+            {
+                "id": 1,
+                "nickname": "xxxx",
+                "observer": false,
+                "ready": false
+            },
+            {
+                "id": 4,
+                "nickname": "whatever",
+                "observer": true,
+                "ready": false
+            }
+        ],
+        "round": {
+          "id": 10,
+          "painter_id": 20,
+          "started_at": "2017-10-05T14:48:00.000Z"
+        }
+    }
+};
+    var painter_id = data.data["round"]["painter_id"];
+    var users = data.data["users"];
+    var userList = $('<div />', {"class":'room-info'});
+    var painter = document.createElement("div");
+    painter.innerHTML = "<a class=\"room-col alignleft\">"+String(painter_id)+"</a>"+"<a class=\"room-col alignright\">Painter</a><br>";
+    $('.room').append(userList);
+    userList.append(painter);
+    
+    for(var i=0;i<users.length;i++) {
+        var usr = document.createElement("div");
+        var role = "   Player";
+        if(users[i]["observer"] == true) {
+            role = "   Observer";
+        }
+        usr.innerHTML = "<a class=\"room-col\">"+String(users[i]["nickname"])+"</a>"+"<a style=\"clear: both;\" class=\"room-col alignright\">"+role+"</a><br>";
+        userList.append(usr);
+    }
+    
+    /*
     console.log('[INFO 1]');
     $.get( "/api/me", function( data ) {
         console.log('data: ' + JSON.stringify(data));
         console.log('data: ' + data['nickname']);
         $('#nameContainer').html(data.nickname);
     });
+    */
 });
 
 $('#logOutButton').click(function() {
