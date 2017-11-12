6dea082a1a49512f627ade20c8b91480d0235e95
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-11 20:43:30 -0500

    socket io lib

--- src/io.js
+++ src/io.js
@@ -1,19 +1,95 @@
 const io = require('socket.io')();
+var db = require('./db');
 
+// two namespaces
+var lounge = io.of('/lounge');
+var room = io.of('/room');
 
-io.use(function (socket, next) {
-  let handshake = socket.handshake;
-  console.log(handshake);
-  return next();
-});
+var socketToUserID = {};
 
+var auth = function (socket, next) {
+  let token = socket.handshake.query.token;
+  db.one('SELECT * FROM users WHERE token = $1', token || 'token_missing')
+    .then(function (user) {
+      socketToUserID[socket.id] = user.id;
+      return next();
+    })
+    .catch(function (err) {
+      return next();
+    });
+};
 
-io.on('connection', function(socket){
-  console.log('a client connected')
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+lounge.use(auth);
+room.use(auth);
+
+lounge.on('connection', function(socket) {
+
+  if (socketToUserID[socket.id] == undefined)
+  {
+    socket.disconnect();
+    console.log("disconnect due to auth failure");
+    return;
+  }
+
+  var socketId = socket.id;
+  var userId = socketToUserID[socket.id];
+
+  console.log("user", userId, "connected to /lounge");
+
+  socket.on('disconnecting', function(reason) {
+    console.log("socket " + socketId + " disconnecting");
+    delete socketToUserID[socketId];
   });
+
+  socket.on('error', function(reason) {
+    console.log("socket " + socketId + " error");
+    delete socketToUserID[socketId];
+  });
+
 });
 
 
-module.exports = io;
+room.on('connection', function(socket){
+
+  if (socketToUserID[socket.id] == undefined)
+  {
+    socket.disconnect(true);
+    console.log("disconnect due to auth failure");
+    return;
+  }
+
+  var socketId = socket.id;
+  var userId = socketToUserID[socket.id];
+
+  console.log("user", userId, "connected to /room");
+
+  socket.on('disconnecting', function(reason) {
+    console.log("socket " + socketId + " disconnecting");
+    delete socketToUserID[socketId];
+  });
+
+  socket.on('error', function(reason) {
+    console.log("socket " + socketId + " error");
+    delete socketToUserID[socketId];
+  });
+
+  db.oneOrNone('SELECT room_id FROM users WHERE id = $1', userId, r => r.room_id)
+    .then(function (roomId) {
+      console.log("user", userId, "in room", roomId);
+      if (roomId == null)
+      {
+        console.log("disconnect due to not in a room");
+        socket.disconnect(true);
+      } else {
+        socket.join("room_" + roomId);
+      }
+    });
+
+});
+
+
+module.exports = {
+  lounge: lounge,
+  room: room,
+  io: io
+};
