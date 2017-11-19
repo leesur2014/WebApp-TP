1398f4a0b0b5252891f2dd9bfdfabaeae4c73a8e
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-19 10:06:05 -0500

    remove socketIdtoUserId

--- src/io.js
+++ src/io.js
@@ -5,102 +5,66 @@ var db = require('./db');
 var lounge = io.of('/lounge');
 var room = io.of('/room');
 
-var socketToUserID = {};
-
-var auth = function (socket, next) {
+function getUser(socket) {
   let token = socket.handshake.query.token;
-  db.one('SELECT * FROM users WHERE token = $1 AND online = TRUE', token || 'token_missing')
-    .then(function (user) {
-      socketToUserID[socket.id] = user.id;
-      return next();
-    })
-    .catch(function (err) {
-      return next();
-    });
+  return db.one('SELECT * FROM users WHERE token = $1 AND online = TRUE', token || 'token_missing');
 };
 
-lounge.use(auth);
-room.use(auth);
-
 lounge.on('connection', function(socket) {
+  getUser(socket)
+    .then(function (user) {
+      console.log("user", user.id, "connected to /lounge");
 
-  if (socketToUserID[socket.id] == undefined)
-  {
-    socket.disconnect();
-    console.log("disconnect due to auth failure");
-    return;
-  }
-
-  var socketId = socket.id;
-  var userId = socketToUserID[socket.id];
-
-  console.log("user", userId, "connected to /lounge");
-
-  socket.on('packet', function () {
-    console.log("socket", socketId, "ping");
-    db.proc('user_ping', userId)
-      .catch(function (e) {
-        console.log(e);
+      socket.conn.on('packet', function () {
+        console.log("socket", socket.id, "ping");
+        db.proc('user_ping', user.id)
+          .catch(function (e) {
+            console.log(e);
+          });
       });
-  });
 
-  socket.on('disconnecting', function(reason) {
-    console.log("socket", socketId, "disconnecting");
-    delete socketToUserID[socketId];
-  });
+      socket.on('disconnecting', function(reason) {
+        console.log("socket", socket.id, "disconnecting");
+      });
 
-  socket.on('error', function(reason) {
-    console.log("socket", socketId, "error");
-    delete socketToUserID[socketId];
-  });
+      socket.on('error', function(reason) {
+        console.log("socket", socket.id, "error");
+      });
+    })
+    .catch(function (e) {
+      console.log("disconnect due to auth failure");
+      socket.disconnect(true);
+    });
 
 });
 
 
-room.on('connection', function(socket){
+room.on('connection', function(socket) {
+  getUser(socket)
+    .then(function (user) {
+      console.log("user", user.id, "connected to /room");
 
-  if (socketToUserID[socket.id] == undefined)
-  {
-    socket.disconnect(true);
-    console.log("disconnect due to auth failure");
-    return;
-  }
-
-  var socketId = socket.id;
-  var userId = socketToUserID[socket.id];
-
-  console.log("user", userId, "connected to /room");
-
-  socket.on('packet', function () {
-    console.log("socket", socketId, "ping");
-    db.proc('user_ping', userId)
-      .catch(function (e) {
-        console.log(e);
+      socket.on('disconnecting', function(reason) {
+        console.log("socket", socket.id, "disconnecting");
       });
-  });
 
-  socket.on('disconnecting', function(reason) {
-    console.log("socket", socketId, "disconnecting");
-    delete socketToUserID[socketId];
-  });
+      socket.on('error', function(reason) {
+        console.log("socket ", socket.id, "error");
+      });
 
-  socket.on('error', function(reason) {
-    console.log("socket ", socketId, "error");
-    delete socketToUserID[socketId];
-  });
-
-  db.oneOrNone('SELECT room_id FROM users WHERE id = $1', userId, r => r.room_id)
-    .then(function (roomId) {
-      console.log("user", userId, "in room", roomId);
-      if (roomId == null)
+      if (user.room_id == null)
       {
-        console.log("disconnect due to not in a room");
+        console.log("user", user.id, "not in a room, disconnect");
         socket.disconnect(true);
       } else {
-        socket.join("room_" + roomId);
+        console.log("user join room", user.room_id)
+        socket.join("room_" + user.room_id);
       }
+    })
+    .catch(function (e) {
+      console.log("disconnect due to auth failure");
+      socket.disconnect(true);
     });
-
 });
 
 
