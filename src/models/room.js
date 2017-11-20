fd1809e7fa9378d7a6f88340b86f265af69beb5b
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-19 20:17:40 -0500

    add round timeout auto ending

--- src/models/room.js
+++ src/models/room.js
@@ -1,5 +1,4 @@
 var db = require('../db');
-
 var io = require('../io');
 
 Room = {};
@@ -30,9 +29,44 @@ Room.getInfoById = async function (room_id) {
 Room.startNewRound = function (room_id) {
   return db.proc('room_start_round', room_id)
     .then(function (round) {
-      io.room.to('room_' + room_id)
-        .emit('round_start', {round_id: round.id});
-      return null;
+      io.room.to('room_' + room_id).emit('round_start', {round_id: round.id});
+
+      var seconds = 60;
+      var timer = setInterval(function () {
+        db.proc("round_get_by_id", round.id)
+          .then(function(round) {
+            seconds--;
+            if (seconds <= 0)
+            {
+              // timeout, end this round
+              console.log("round", round.id, "timed out");
+              clearInterval(timer);
+              db.proc('round_end', round_id)
+                .then(function(round) {
+                  io.room.to('room_' + round.room_id).emit('round_end', {round_id: round.id});
+                })
+                .catch(function(e) {
+                  // this is not an error
+                  console.log(e);
+                });
+              return;
+            }
+            if (round.ended_at === null)
+            {
+              // the round is still open, send count_down event to clients
+              console.log("round", round.id, "has", seconds, "left");
+              io.room.to('room_' + room_id).emit('count_down', {seconds: seconds});
+            } else {
+              console.log("round", round.id, "ended prematurely");
+              clearInterval(timer);
+            }
+          })
+          .catch(function(e) {
+            console.log(e);
+            clearInterval(timer);
+          })
+      }, 1000);
+      return round;
     })
     .catch(function (err) {
       // this is not an error
