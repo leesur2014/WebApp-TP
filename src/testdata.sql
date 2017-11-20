269315a6ce98f797008b82ee68db9b7a37c5418b
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2017-11-20 12:39:22 -0500

    expand testdata - dictionary

--- src/testdata.sql
+++ src/testdata.sql
@@ -3,4 +3,4 @@ INSERT INTO users (fb_id, nickname) VALUES ('test_id_3', 'Test User 3') RETURNIN
 INSERT INTO users (fb_id, nickname) VALUES ('test_id_1', 'Test User 1') RETURNING id;
 
 
-INSERT INTO dictionary VALUES ('apple'), ('orange'), ('greet'), ('dictionary'), ('keyboard');
+INSERT INTO dictionary VALUES ('apple'), ('orange'), ('greet'), ('dictionary'), ('keyboard'), ('book'), ('business'), ('case'), ('child'), ('company'), ('country'), ('day'), ('eye'), ('fact'), ('family'), ('government'), ('group'), ('hand'), ('home'), ('job'), ('life'), ('lot'), ('man'), ('money'), ('month'),('mother'), ('number'), ('people'), ('place'), ('point'), ('problem'), ('program'), ('question'), ('right'), ('room'), ('school'), ('state'), ('story'), ('student'), ('study'), ('system'), ('time'), ('water'), ('way'), ('week'), ('woman'), ('word'), ('work'), ('world'), ('year');
