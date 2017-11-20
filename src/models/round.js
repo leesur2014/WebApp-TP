647cf365fe6bea23892cb3b19a07cd23ae609ba6
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-19 23:35:21 -0500

    move room.startnewround to round.tryToStart()

--- src/models/round.js
+++ src/models/round.js
@@ -29,4 +29,51 @@ Round.tryToEnd = function (round_id) {
     });
 };
 
+
+Round.tryToStart = function (room_id) {
+  return db.proc('room_start_round', room_id)
+    .then(function (round) {
+      // reach here if new round is started
+      io.lounge.emit('room_change', {room_id: room_id});
+      io.room.to('room_' + room_id).emit('round_start', {round_id: round.id});
+
+      var seconds = 60;
+      var timer = setInterval(function () {
+        db.one("SELECT * FROM open_rounds WHERE id = $1", round.id)
+          .then(function(round) {
+            seconds--;
+            if (seconds <= 0)
+            {
+              // timeout, end this round
+              console.log("round", round.id, "timed out");
+              clearInterval(timer);
+              db.proc('round_end', round.id)
+                .then(function() {
+                  io.room.to('room_' + round.room_id).emit('round_end', {round_id: round.id});
+                })
+                .catch(function(e) {
+                  // reach here if the round is already ended
+                  // this is not an error
+                });
+            } else {
+              console.log("round", round.id, "has", seconds, "left");
+              io.room.to('room_' + room_id).emit('count_down', {seconds: seconds});
+            }
+          })
+          .catch(function(e) {
+            // reach here if the round is already ended
+            // or an error happens
+            console.log(e);
+            clearInterval(timer);
+          });
+      }, 1000);
+      return round;
+    })
+    .catch(function (err) {
+      // reach here if new round cannot be started
+      // this is not an error
+      return null
+    });
+};
+
 module.exports = Round;
