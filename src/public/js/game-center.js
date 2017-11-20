9ec0b17cb66aea19dc02af19fbeab43d8a4bb255
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-20 11:15:35 -0500

    change game center UI

--- src/public/js/game-center.js
+++ src/public/js/game-center.js
@@ -1,118 +1,73 @@
 $(document).ready(function() {
-    $.get("/api/me", function(data) {
-        var token = data.token;
-        $('#nickname').text(data.nickname);
-        // display: if the user is in a room
-        if (data.room_id != null) {
+    $.get("/api/me", function(user) {
+
+        $('#nickname').text(user.nickname);
+
+        if (user.room_id != null) {
             console.log("redirect user to room page");
             window.location.pathname = '/room';
         }
 
-        var socket = io('/lounge?token='+ token);
+        var socket = io('/lounge?token=' + user.token);
+
         socket.on('room_create', function(msg) {
-            console.log('[INFO] Created - room Id: ' + msg.room_id);
             var room_id = msg.room_id;
-            $.get('/api/room/' + room_id, function(new_room_data) {
-                console.log('[INFO] New room has been created: ' + JSON.stringify(new_room_data));
-                $('#roomContainer').prepend(generate_room(new_room_data['data']));
+            $.get('/api/room/' + room_id, function(resp) {
+                $('#room-table').prepend(generate_room(resp.data));
             });
         });
+
         socket.on('room_change', function(msg) {
-            console.log('[INFO] Changed - room Id: ' + msg.room_id);
             var room_id = msg.room_id;
-            var num_rooms = $('#roomContainer').children().length;
-            // delete original room
-            for (var i = 0; i < num_rooms; ++i) {
-                var this_room = $('#roomContainer .room:nth-child(' + (i + 1) + ')');
-                var this_id = this_room.attr('id');
-                console.log('[INFO] This room id: ' + this_id);
-                if (this_id == room_id) {
-                    this_room.remove();
-                    break;
-                }
-            }
-
             // add a new room
-            $.get('/api/room/' + room_id, function(new_room_data) {
-                console.log('[INFO] New room has been created: ' + JSON.stringify(new_room_data));
-                $('#roomContainer').prepend(generate_room(new_room_data['data']));
+            $.get('/api/room/' + room_id, function(resp) {
+                $('#room-table').prepend(generate_room(resp.data));
             });
         });
-        socket.on('room_delete', function(msg) {
-            console.log('[INFO] Deleted - room Id: ' + msg.room_id);
-            var num_rooms = $('#roomContainer').children().length;
-            console.log('[INFO] number of rooms: ' + num_rooms);
-            $('#roomContainer').children().each(function(index) {
-                // For debug
-                console.log('[INFO]' + index + ": " + $( this ).attr('id'));
-            });
 
-            for (var i = 0; i < num_rooms; ++i) {
-                var this_room = $('#roomContainer .room:nth-child(' + (i + 1) + ')');
-                var this_id = this_room.attr('id');
-                console.log('[INFO] This room id: ' + this_id);
-                if (this_id == msg.room_id) {
-                    this_room.remove();
-                    break;
-                }
-            }
+        socket.on('room_delete', function(msg) {
+          var room_id = msg.room_id;
+
         });
     });
 
     $.get('/api/lounge', function(data) {
-        console.log('[INFO]all the rooms: ' + JSON.stringify(data));
-        var l = data['data'].length;
+        var l = data.data.length;
         for (var i = 0; i < l; ++i) {
-//            console.log(JSON.stringify(data['data'][i]));
-            var room = generate_room(data['data'][i]);
-            $('#roomContainer').prepend(room);
+            var room = generate_room(data.data[i]);
+            $('#room-table').prepend(room);
         }
-        console.log('[INFO] Initial page: we have: ' + $('#roomContainer').children().length + ' rooms.');
     })
     .fail(function() {
-        alert( "error" );
+        alert("Failed to fetch the public room list");
     });
 });
 
 function generate_room(entry) {
-    var room = $('<div/>');
-    room.attr('id', entry.id);
-    room.addClass('room');
-    // room id
-    room.append($('<div/>').addClass('room_id_container').html('<h2> Room ' + entry.id + '</h2>'));
-    room.append('<hr>');
+    var room = $('<tr>');
+    var td_id = $('<td>');
+    var td_players = $('<td>');
+    var td_observers = $('<td>');
+    var td_actions = $('<td>');
 
-    // # of players & observers
-    room.append($('<div/>').addClass('players_number_container').html('Players: ' + entry.player_count));
-    room.append($('<div/>').addClass('observers_number_container').html('Observers: ' + (entry.user_count - entry.player_count)));
+    td_id.text(entry.id);
+    td_players.text(entry.player_count);
+    td_observers.text(entry.user_count - entry.player_count);
 
-    // is the room playing?
-    var is_playing = entry.round_id != null ? '<span class="label label-default">Playing</span>':'<span class="label label-success">Idle</span>';
-    room.append(is_playing);
+    var join_as_player = $('<button>Join as player</button>');
+    var join_as_observer = $('<button>Join as observer</button>');
+    join_as_player.addClass("btn btn-primary btn-sm");
+    join_as_observer.addClass("btn btn-success btn-sm");
 
-    // join the room button
-    var join_room_form = $('<form/>');
-    join_room_form.addClass('join_room_form');
-    join_room_form.attr('method', 'post');
-    join_room_form.append('<label class="radio-inline"><input type="radio" name="person_type" value = "player" checked>Player</label>');
-    join_room_form.append('<label class="radio-inline"><input type="radio" name="person_type" value = "observer">Observer</label>');
-    join_room_form.append('&nbsp; <input class = "btn btn-primary" type="submit" value="Join">');
+    if (entry.round_id == null)
+    {
+      td_actions.append(join_as_player);
+    }
+    td_actions.append(join_as_observer);
 
-    $(join_room_form).submit(function(event) {
-        event.preventDefault();
-        var v = $(this).find("input[name='person_type']:checked").val();
-        console.log('[INFO] type: ' + v);
-        var parameters = {room_id: $(this).parent().attr('id'), observer: v == 'observer'};
-        console.log('[INFO] join_room parameters' + JSON.stringify(parameters));
-        $.post( "/api/enter", parameters, function( data ) {
-            if (data.code == 0) {
-                console.log('[INFO]enter successfully!');
-                location.href = '/room';
-            } else {
-                alert('[ERROR]' + data.error);
-            }
-        });
-    });
-    room.append(join_room_form);
+    room.append(td_id);
+    room.append(td_players);
+    room.append(td_observers);
+    room.append(td_actions);
     return room;
 }
