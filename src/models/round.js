dd43080f59e03663010485e4f7ff645822641c3c
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-19 20:17:51 -0500

    remove Round.end

--- src/models/round.js
+++ src/models/round.js
@@ -3,20 +3,6 @@ var io = require('../io');
 
 Round = {};
 
-Round.end = function (round_id) {
-  return db.proc('round_end', round_id)
-    .then(function (round) {
-      io.room.to('room_' + round.room_id)
-        .emit('round_end', {round_id: round_id});
-      return null;
-    })
-    .catch(function (err) {
-      // this is an error
-      console.log(err);
-      return null;
-    });
-};
-
 Round.getInfoByUserId = function (user_id) {
   return db.proc("user_get_current_round", [user_id])
     .then(function (round) {
