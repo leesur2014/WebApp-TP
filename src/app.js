791c789d3ff9fa606c54e46aa462da8b84bb8880
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-20 14:33:05 -0500

    move routes/index.js into app.js

--- src/app.js
+++ src/app.js
@@ -4,39 +4,25 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var passport = require('passport');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
-
-var passport = require('passport');
 
 var app = express();
 
 var api = require('./routes/api');
 var users = require('./routes/users');
 
-// // view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-var http = require('http').Server(app);
 
-var index = require('./routes/index');
-var api = require('./routes/api');
-var users = require('./routes/users');
+var http = require('http').Server(app);
 
 var io = require('./io').io;
 io.attach(http);
 
 app.use(logger('dev'));
-
-// NOTE remove in production
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 
 app.use(session({
     store: new RedisStore({
@@ -49,14 +35,30 @@ app.use(session({
 }));
 
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', index);
 app.use('/users', users);
 app.use('/api', api);
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
+router.get('/',function(req,res) {
+    if (!req.isAuthenticated())
+    {
+        // if not authenticated, render login view
+        return res.render('index');
+    }
+    if (req.user.room_id != null)
+    {
+        // render room view if the user is in a room
+        return res.render('room');
+    }
+    else
+    {
+        // render the game-center view if not in a room
+        return res.render('game-center');
+    }
+});
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
