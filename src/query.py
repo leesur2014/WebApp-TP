b518215b42dbbb9b8c2054929c360fc481e58e1a
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2017-11-21 09:35:27 -0500

    created - script for scanning csv file

--- src/query.py
+++ src/query.py
@@ -1,1 +1,11 @@
 # render and extract all words in csv file
+import csv
+
+def main():
+    with open('/Users/xuhuiwang/Documents/words.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print('Number of elements:', len(row))
+            print ', '.join(row)
+if __name__ == "__main__":
+    main()
