94565e80ba9a4b66d05b5abcb30be79f438e60fc
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2017-11-21 09:48:46 -0500

    example scripts for extracting words

--- src/query.py
+++ src/query.py
@@ -1,11 +1,17 @@
-# render and extract all words in csv file
 import csv
 
 def main():
+    my_words = []
     with open('/Users/xuhuiwang/Documents/words.csv', 'rb') as f:
         reader = csv.reader(f)
         for row in reader:
-            print('Number of elements:', len(row))
-            print ', '.join(row)
+            # print('Number of elements:', len(row))
+            # append all nouns
+            if 'n' in row[1] and row[0] not in my_words:
+                my_words.append((row[0]))
+
+    print("'), ('".join(my_words))
+
+
 if __name__ == "__main__":
     main()
