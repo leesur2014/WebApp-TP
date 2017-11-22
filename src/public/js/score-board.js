b9aae051eb02ed7b0aa90f9ed1d183cd6129033a
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2017-11-22 13:29:14 -0500

    initialize - for an use's score

--- src/public/js/score-board.js
+++ src/public/js/score-board.js
@@ -1,5 +1,9 @@
 $(function () {
-    alert('hah');
-  // TODO: load score board from server
-
+    $.get('/api/top-users', function(data) {
+        console.log('[INFO] Top user data: ' + JSON.stringify(data));
+        for (var i in data.data) {
+            console.log('[INFO] This user: ' + JSON.stringify(data.data[i]));
+            var this_entry = $('<tr/>');
+        }
+    });
 });
