fc27bbfb127f585866c55d4f57a5d970bfc6d37d
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2017-11-22 13:30:49 -0500

    traverse - score array

--- src/public/js/score-board.js
+++ src/public/js/score-board.js
@@ -2,8 +2,10 @@ $(function () {
     $.get('/api/top-users', function(data) {
         console.log('[INFO] Top user data: ' + JSON.stringify(data));
         for (var i in data.data) {
-            console.log('[INFO] This user: ' + JSON.stringify(data.data[i]));
+            var this_score = data.data[i];
+            console.log('[INFO] This user: ' + JSON.stringify(this_score));
             var this_entry = $('<tr/>');
+//            this_entry
         }
     });
 });
