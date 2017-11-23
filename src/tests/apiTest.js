5dce204309fa28b8641e068bc4bfe7dc62b54e9c
leesur2014 <542785042@qq.com>
2017-11-23 17:17:41 -0500

    Added test cases.

--- src/tests/apiTest.js
+++ src/tests/apiTest.js
@@ -48,10 +48,8 @@ it('should get user info /api/me GET', function(done) {
             res.body.should.have.property('online');
             res.body.online.should.equal(true);
             done();
-
         });
-
-    });
+});
 
 it('should list all rooms /api/lounge GET', function(done) {
    request(Url)
@@ -176,22 +174,110 @@ it('should exit current room /api/exit POST', function(done) {
 });
 
 
+var cookie4 = 'nXV_-CsOG8PkCFosAAAA';
+var cookie5 = 'AwrHXaiD0A8gtU0S9aEGgrtjQcbUC22aXpUiyCldjZerPFa01xouJ44wj66oKcii';
+it('should get info about a public room /api/room/{room_id} GET', function(done) {
+  request(Url)
+  .get('/api/room/3')
+  .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*\/*;q=0.8')
+  .set('Content-Type', 'application/json')
+  .set('Accept-Encoding','gzip, deflate')
+  .set('Accept-Language','zh-CN,zh;q=0.8')
+  .set('Cookie',['connect.sid='+cookie1,'io='+cookie4,'csrftoken='+cookie5]) 
+  .end(function(err,res) {
+    res.should.have.status(200);
+    res.should.be.json;
+    res.body.should.have.property('data');
+    res.body.data.should.have.property('id');
+    res.body.data.should.have.property('user_count');
+    res.body.data.should.have.property('player_count');
+    res.body.data.should.have.property('round_id');
+    done();
+  });
+});
+
+it('should get a certain user info /api/user/{user_id} GET', function(done) {
+   request(Url)
+           .get('/api/user/1')
+           .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*\/*;q=0.8')
+           .set('Content-Type', 'application/json')
+           .set('Accept-Encoding','gzip, deflate')
+           .set('Accept-Language','zh-CN,zh;q=0.8')
+           .set('Cookie',['connect.sid='+cookie1,'io='+cookie4,'csrftoken='+cookie5])
+        .end(function(err,res) {
+            //console.log(res);
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.have.property('code');
+            res.body.should.have.property('data');
+            res.body.data.should.have.property('id');
+            res.body.data.id.should.equal(1);
+            res.body.data.should.have.property('nickname');
+            res.body.data.should.have.property('score_draw');
+            res.body.data.should.have.property('score_guess');
+            done();
+        });
+});
+
+//it('should get info of past round /api/round/{round_id} GET', function(done) {
+//   request(Url)
+//           .get('/api/round/1')
+//           .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*\/*;q=0.8')
+//           .set('Content-Type', 'application/json')
+//           .set('Accept-Encoding','gzip, deflate')
+//           .set('Accept-Language','zh-CN,zh;q=0.8')
+//           .set('Cookie',['connect.sid='+cookie1,'io='+cookie2,'csrftoken='+cookie3])
+//        .end(function(err,res) {
+//            //console.log(res);
+//            res.should.have.status(200);
+//            res.should.be.json;
+//            res.body.should.have.property('code');
+//            res.body.should.have.property('data');
+//            res.body.data.should.have.property('id');
+//            res.body.data.id.should.equal(1);
+//            res.body.data.should.have.property('painter_id');
+//            res.body.data.should.have.property('room_id');
+//            res.body.data.should.have.property('started_at');
+//            res.body.data.should.have.property('ended_at');
+//            res.body.data.should.have.property('answer');
+//            done();
+//        });
+//});
+
+//it('should join a room /api/enter POST', function(done) {
+//  request(Url)
+//  .post('/api/enter')
+//  .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*\/*;q=0.8')
+//  .set('Content-Type', 'application/json')
+//  .set('Accept-Encoding','gzip, deflate')
+//  .set('Accept-Language','zh-CN,zh;q=0.8')
+//  .set('Cookie',['connect.sid='+cookie1,'io='+cookie2,'csrftoken='+cookie3])
+//  .end(function(err,res) {
+//    res.should.have.status(200);
+//    res.should.be.json;
+//    res.body.should.have.property('code');
+//    res.body.should.have.property('room_id');
+//
+//  });
+//});
+
+
 });
 
 /*
 Method | URL | Description
 ---|----|-------------
-GET | `/api/me` | Get current user's info
-POST | `/api/me` | change my nickname
-GET | `/api/lounge` | Get a list of public rooms
-GET | `/api/room` | Get detailed info about current room
-GET | `/api/room/{room_id}` | Get detailed info about a public room
-GET | `/api/user/{user_id}` | Get info about a user
-GET | `/api/round` | Get detailed info about current round
-GET | `/api/round/{round_id}` | Get result of a past round
-POST | `/api/room` | Create a room
+-GET | `/api/me` | Get current user's info
+-POST | `/api/me` | change my nickname
+-GET | `/api/lounge` | Get a list of public rooms
+-GET | `/api/room` | Get detailed info about current room
+-GET | `/api/room/{room_id}` | Get detailed info about a public room
+-GET | `/api/user/{user_id}` | Get info about a user
+/GET | `/api/round` | Get detailed info about current round
+-GET | `/api/round/{round_id}` | Get result of a past round
+-POST | `/api/room` | Create a room
 POST | `/api/enter` | Join a room
-POST | `/api/exit` | Quit a room
+-POST | `/api/exit` | Quit a room
 POST | `/api/ready` | Set/clear the user's ready bit
 POST | `/api/guess` | Submit the answer for a round
 POST | `/api/draw` | Send the painter's drawing
