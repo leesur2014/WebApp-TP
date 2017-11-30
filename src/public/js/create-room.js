8fd4179c9d6b7babb0ed452a54aada7bac14bf6b
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-30 11:47:53 -0500

    butter UI when creating a room

--- src/public/js/create-room.js
+++ src/public/js/create-room.js
@@ -1,14 +1,26 @@
 $('#create-room-form').submit(function(event) {
-    event.preventDefault();
-    var data = {
-      passcode: this.passcode.value
-    };
-    $.post("/api/room", data, function(resp) {
-        if (resp.code == 0) {
-            //room created
-            location.reload();
-        } else {
-          alert('Error: ' + resp.error);
-        }
-    });
+  event.preventDefault();
+  var data = {
+    passcode: this.passcode.value
+  };
+  $.post("/api/room", data, function(resp) {
+      if (resp.code == 0) {
+          //room created
+          location.reload();
+      } else {
+        alert('Error: ' + resp.error);
+      }
+  });
+});
+
+
+$(document).ready(function() {
+  $('input[type=radio][name=room-type]').change(function() {
+    if (this.value == 'private') {
+      $("#room-password-div").show();
+    } else {
+      $("#room-password-div").hide();
+      $("#room-password-input").val("");
+    }
+  });
 });
