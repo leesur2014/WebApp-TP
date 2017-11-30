0a78d11e80387fccfe69d6acbb91f13c4bf27b53
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-30 10:04:07 -0500

    add history page

---
+++ src/public/js/history.js
@@ -0,0 +1,29 @@
+$(function () {
+
+  return;
+  
+  $.get('/api/top-users', function(res) {
+    for(var i = 0; i < res.data.length; i++) {
+        var row = $('<tr>');
+
+        var nickname = $('<td>');
+        nickname.text(res.data[i].nickname);
+        row.append(nickname);
+
+        var score_draw = $('<td>');
+        score_draw.text(res.data[i].score_draw);
+        row.append(score_draw);
+
+        var score_guess = $('<td>');
+        score_guess.text(res.data[i].score_guess);
+        row.append(score_guess);
+
+        var score_total = $('<td>');
+        score_total.text(res.data[i].score);
+        row.append(score_total);
+
+        $('#rank-table').append(row);
+    }
+  });
+
+});
