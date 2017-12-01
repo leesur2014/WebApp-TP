15951e0e6775fa573dcb4f1a94f7670f7a9d1b0e
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-30 19:36:05 -0500

    show user's ranking in profile

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -1,13 +1,28 @@
-$('#change-nickname-form').submit(function(event) {
-    event.preventDefault();
-    var data = {
-      nickname: this.nickname.value
-    };
-    $.post("/api/me", data, function(resp) {
-        if (resp.code == 0) {
-            location.reload();
-        } else {
-            alert('Error: ' + resp.error);
-        }
-    });
+
+$(function () {
+
+  $('#change-nickname-form').submit(function(event) {
+      event.preventDefault();
+      var data = {
+        nickname: this.nickname.value
+      };
+      $.post("/api/me", data, function(resp) {
+          if (resp.code == 0) {
+              location.reload();
+          } else {
+              alert('Error: ' + resp.error);
+          }
+      });
+  });
+
+  $("#profile-modal").on("show.bs.modal", function () {
+    $.getJSON("/api/rank", function (resp) {
+      if (resp.code != 0) {
+        alert("Error: " + resp.error);
+      } else {
+        $("#user-ranking").text(`${resp.data.rank} / ${resp.data.total}`);
+      }
+    })
+  });
+  
 });
