bf721be9f560566df07028e8d911eecff910a91a
Jim Zhong <jimzhong@users.noreply.github.com>
2017-11-30 19:38:25 -0500

    show user's ranking in scoreboard

--- src/public/js/score-board.js
+++ src/public/js/score-board.js
@@ -6,26 +6,14 @@ $(function () {
       alert("Error: " + res.error);
       return;
     }
-    
+
     for(var i = 0; i < res.data.length; i++) {
         var row = $('<tr>');
-
-        var nickname = $('<td>');
-        nickname.text(res.data[i].nickname);
-        row.append(nickname);
-
-        var score_draw = $('<td>');
-        score_draw.text(res.data[i].score_draw);
-        row.append(score_draw);
-
-        var score_guess = $('<td>');
-        score_guess.text(res.data[i].score_guess);
-        row.append(score_guess);
-
-        var score_total = $('<td>');
-        score_total.text(res.data[i].score);
-        row.append(score_total);
-
+        row.append($('<td>').text(res.data[i].rank));
+        row.append($('<td>').text(res.data[i].nickname));
+        row.append($('<td>').text(res.data[i].score_draw));
+        row.append($('<td>').text(res.data[i].score_guess));
+        row.append($('<td>').text(res.data[i].score));
         $('#rank-table').append(row);
     }
   });
