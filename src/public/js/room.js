af87d3e66baa49896de29e13c031b029520555f2
Jim Zhong <jimzhong@users.noreply.github.com>
2017-12-01 09:51:54 -0500

    add socket.io error handlers

--- src/public/js/room.js
+++ src/public/js/room.js
@@ -228,6 +228,15 @@ $(function () {
     socket.on('reconnect', function (attempt) {
       hide_alert();
     });
+
+    socket.on('disconnect', function () {
+      show_alert('Disconnected from the server. Please refresh the page.');
+    })；
+
+    socket.on('error', function (error) {
+      show_alert('An error occurred. Please refresh the page.');
+    })；
+
   }
 
 
